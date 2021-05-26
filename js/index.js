b9338a746d695a4ba6b8092c993378ac3072d480
Sucs-God <technogen228@gmail.com>
2021-05-27 01:54:41 +0300

    [code] Lesson - Closures and practice

--- js/index.js
+++ js/index.js
@@ -24,38 +24,38 @@
 //
 // const str = new String('I am string')
 
-// 2 This
-function hello() {
-    console.log('Hello', this)
-}
-
-const person = {
-    name: 'Vladilen',
-    age: 25,
-    sayHello: hello,
-    sayHelloWindow: hello.bind(document),
-    logInfo: function (job, phone) {
-        console.group(`${this.name} info:`)
-        console.log(`Name is ${this.name}`)
-        console.log(`Age is ${this.age}`)
-        console.log(`Job is ${job}`)
-        console.log(`Phone is ${phone}`)
-        console.groupEnd()
-    }
-}
-
-const lena = {
-    name: 'Elena',
-    age: 23
-}
+// 2 This additionally
+// function hello() {
+//     console.log('Hello', this)
+// }
+//
+// const person = {
+//     name: 'Vladilen',
+//     age: 25,
+//     sayHello: hello,
+//     sayHelloWindow: hello.bind(document),
+//     logInfo: function (job, phone) {
+//         console.group(`${this.name} info:`)
+//         console.log(`Name is ${this.name}`)
+//         console.log(`Age is ${this.age}`)
+//         console.log(`Job is ${job}`)
+//         console.log(`Phone is ${phone}`)
+//         console.groupEnd()
+//     }
+// }
+//
+// const lena = {
+//     name: 'Elena',
+//     age: 23
+// }
 
 // person.logInfo.bind(lena, 'Frontend', '8-999-123-12-23')()
 // person.logInfo.call(lena, 'Frontend', '8-999-123-12-23')
-person.logInfo.apply(lena, ['Frontend', '8-999-123-12-23'])
+// person.logInfo.apply(lena, ['Frontend', '8-999-123-12-23'])
 
 /// ================ More about prototypes using an example
 
-const array = [1, 2, 3, 4, 5]
+// const array = [1, 2, 3, 4, 5]
 
 // function multBy(arr, n) {
 //     return arr.map(function (i) {
@@ -63,10 +63,83 @@ const array = [1, 2, 3, 4, 5]
 //     })
 // }
 
-Array.prototype.multBy = function (n) {
-    return this.map(function (i) {
-        return i * n
-    })
+// Array.prototype.multBy = function (n) {
+//     return this.map(function (i) {
+//         return i * n
+//     })
+// }
+//
+// console.log(array.multBy(20))
+
+// 3 Closures
+
+// function createCalcFunction(n) {
+//     return function () {
+//         console.log(1000* n)
+//     }
+// }
+//
+// const calc = createCalcFunction(42)
+// calc()
+
+// function createIncrementor(n) {
+//     return function (num) {
+//         return n + num
+//     }
+// }
+//
+// const addOne = createIncrementor(1)
+// const addTen = createIncrementor(10)
+//
+// console.log(addOne(10))
+// console.log(addOne(41))
+//
+// console.log(addTen(10))
+// console.log(addTen(41))
+
+// function urlGenerator(domain) {
+//     return function (url) {
+//         return `https://${url}.${domain}`
+//     }
+// }
+//
+// const comUrl = urlGenerator('com')
+// const ruUrl = urlGenerator('ru')
+//
+// console.log(comUrl('google'))
+// console.log(comUrl('netflix'))
+//
+// console.log(ruUrl('yandex'))
+// console.log(ruUrl('vkontakte'))
+//
+// console.log(comUrl('vkontakte'))
+
+// Practice
+/*
+    Написать свою функцию bind
+    Пример работы:
+
+    function logPerson() {
+        console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+    }
+
+    const person1 = {name: 'Михаил', age: 22, job: 'Frontend'}
+    const person2 = {name: 'Елена', age: 19, job: 'SMM'}
+
+    bind(person1, logPerson)
+    bind(person2, logPerson)
+ */
+function bind(contex, fn) {
+    return function (...args) {
+        fn.call(contex, args)
+    }
 }
 
-console.log(array.multBy(20))
+function logPerson() {
+    console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+}
+const person1 = {name: 'Михаил', age: 22, job: 'Frontend'}
+const person2 = {name: 'Елена', age: 19, job: 'SMM'}
+
+bind(person1, logPerson)()
+bind(person2, logPerson)()
