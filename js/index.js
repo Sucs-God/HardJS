ac62505e3e268c0c8b7080a6e06d38ddacd36c02
Sucs-God <technogen228@gmail.com>
2021-05-28 00:36:10 +0300

    [code] Lesson - 4 Asynchrony

--- js/index.js
+++ js/index.js
@@ -129,17 +129,40 @@
     bind(person1, logPerson)
     bind(person2, logPerson)
  */
-function bind(contex, fn) {
-    return function (...args) {
-        fn.call(contex, args)
-    }
+// function bind(contex, fn) {
+//     return function (...args) {
+//         fn.call(contex, args)
+//     }
+// }
+//
+// function logPerson() {
+//     console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+// }
+// const person1 = {name: 'Михаил', age: 22, job: 'Frontend'}
+// const person2 = {name: 'Елена', age: 19, job: 'SMM'}
+//
+// bind(person1, logPerson)()
+// bind(person2, logPerson)()
+
+// 4 Asynchrony
+
+console.log('Start')
+
+console.log('Start 2')
+
+function timeout2sec() {
+    console.log('timeout2sec')
 }
 
-function logPerson() {
-    console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
-}
-const person1 = {name: 'Михаил', age: 22, job: 'Frontend'}
-const person2 = {name: 'Елена', age: 19, job: 'SMM'}
+window.setTimeout(function () {
+    console.log('Inside timeout, after 2 seconds')
+}, 5000)
 
-bind(person1, logPerson)()
-bind(person2, logPerson)()
+window.setTimeout(timeout2sec, 2000)// Если будет setTimeout 0, то вывод функции будет
+// после обработки синхронных команд
+// После того как команда отдаётся на выполнение сторонним api и после получения ответа на этот запрос,
+// функция передаётся в очередь - event loop(цикл событий), и как только стек освобождается, JS проходится
+// по очереди и помещает функцию из очереди в стек
+
+console.log('End')
+
