23754c601e4d822b6446e92c40d54ef34f6b17e3
Sucs-God <technogen228@gmail.com>
2021-05-29 01:46:56 +0300

    [code] Lesson - 6 Flexible configuration of objects

--- js/index.js
+++ js/index.js
@@ -168,7 +168,7 @@
 
 // 5 Promise
 
-console.log('Request data...')
+// console.log('Request data...')
 
 // setTimeout(() => {
 //     console.log('Preparing data...')
@@ -216,21 +216,65 @@ console.log('Request data...')
 //     .catch(err => console.error('Error: ', err))
 //     .finally(() => console.log('Finally'))
 
-const sleep = ms => {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(), ms)
-    })
-}
+// const sleep = ms => {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve(), ms)
+//     })
+// }
 
 // sleep(2000).then(() => console.log('After 2 sec'))
 // sleep(3000).then(() => console.log('After 3 sec'))
 
-Promise.all([sleep(2000), sleep(5000)])
-    .then(() => {
-        console.log('All promises')
-    })
+// Promise.all([sleep(2000), sleep(5000)])
+//     .then(() => {
+//         console.log('All promises')
+//     })
 
-Promise.race([sleep(2000), sleep(5000)])
-    .then(() => {
-        console.log('Race promises')
-    })
+// Promise.race([sleep(2000), sleep(5000)])
+//     .then(() => {
+//         console.log('Race promises')
+//     })
+
+// 6 Flexible configuration of objects
+// Цикл for не сможет итерировать эти поля
+const person = Object.create({
+    calculateAge() {
+        console.log('Age: ', new Date().getFullYear() - this.birthYear)
+    }
+}, {
+    name: {
+        value: 'Vladilen',
+        enumerable: true, // цикл for видит это поле
+        writable: true, // этот поле можно изменять
+        configurable: true // это поле можно удалить
+    },
+    birthYear: {
+        value: 1993,
+        enumerable: false,
+        writable: false,
+        configurable: false
+    },
+    age: {
+        get() {
+            return new Date().getFullYear() - this.birthYear
+        },
+        set(value) {
+            document.body.style.background = 'red'
+            console.log('Set age', value)
+        }
+    }
+})
+
+// А в этом примере цикл for отработает как обычно
+// const person = {
+//     name: 'Vladilen',
+//     birthYear: 1993
+// }
+
+// person.name = 'Maxim'
+
+for (let key in person) {
+    if (person.hasOwnProperty(key)) {// Эта проверка сделана для того,
+        console.log('Key', key, person[key])// что цикл 'for in' не итерировал прототип
+    }
+}
