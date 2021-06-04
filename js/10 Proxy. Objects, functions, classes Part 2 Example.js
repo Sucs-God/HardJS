cacfdd377ab27d496011ea87b9fed427606a4d6e
Sucs-God <technogen228@gmail.com>
2021-06-05 02:34:23 +0300

    [code] Lesson - 11 Generators. Symbol iterator, for of

---
+++ js/10 Proxy. Objects, functions, classes Part 2 Example.js
@@ -0,0 +1,62 @@
+// 10 Proxy. Objects, functions, classes Part 2 Example
+
+// Wrapper
+const withDefaultValue = (targer, defaultValue = 0) => {
+    return new Proxy(targer, {
+        get: (obj, prop) => (prop in obj ? obj[prop] : defaultValue)
+    })
+}
+
+const position = withDefaultValue({
+        x: 24,
+        y: 42
+    },
+    0)
+
+// console.log(position)
+
+// Hidden properties
+const withHiddenProps = (target, prefix = '_') => {
+    return new Proxy(target, {
+        has: (obj, prop) => prop in obj && !prop.startsWith(prefix),
+        ownKeys: obj => Reflect.ownKeys(obj).filter(p => !p.startsWith(prefix)),
+        get: (obj, prop, receiver) => (prop in receiver ? obj[prop] : void 0)
+    })
+}
+
+const data = withHiddenProps({
+    name: 'Vladilen',
+    age: 25,
+    _uid: '1231231'
+})
+
+// Optimization
+const IndexedArray = new Proxy(Array, {
+    construct(target, [argArray], newTarget) {
+        const index = {}
+        argArray.forEach(item => index[item.id] = item)
+
+        return new Proxy(new target(...argArray), {
+            get(target, p, receiver) {
+                switch (p) {
+                    case 'push':
+                        return item => {
+                            index[item.id] = item
+                            target[p].call(target, item)
+                        }
+                    case 'findById':
+                        return id => index[id]
+                    default:
+                        return target[p]
+                }
+            }
+        })
+    }
+})
+
+const users = new IndexedArray([
+    {id: 11, name: 'Vladilen', job: 'Fullstack', age: 25},
+    {id: 22, name: 'Elena', job: 'Student', age: 22},
+    {id: 33, name: 'Victor', job: 'Backend', age: 23},
+    {id: 44, name: 'Vasilisa', job: 'Teacher', age: 24}
+])
--- js/index.js
+++ js/index.js
@@ -1,61 +1,44 @@
-// 10 Proxy. Objects, functions, classes Part 2 Example
+// 11 Generators. Symbol iterator, for of
 
-// Wrapper
-const withDefaultValue = (targer, defaultValue = 0) => {
-    return new Proxy(targer, {
-        get: (obj, prop) => (prop in obj ? obj[prop] : defaultValue)
-    })
-}
+// function* strGenerator() {
+//     yield 'H'
+//     yield 'e'
+//     yield 'l'
+//     yield 'l'
+//     yield 'o'
+// }
+//
+// const str = strGenerator()
 
-const position = withDefaultValue({
-        x: 24,
-        y: 42
-    },
-    0)
+// function* numberGen(n = 10) {
+//     for(let i = 0; i < n; i++) {
+//         yield i
+//     }
+// }
+//
+// const num = numberGen(17)
 
-// console.log(position)
+// const iterator = {
+//     [Symbol.iterator](n = 10) {
+//         let i = 0
+//
+//         return {
+//             next() {
+//                 if (i < n) {
+//                     return {value: ++i, done: false }
+//                 }
+//                 return {value: undefined, done: true }
+//             }
+//         }
+//     }
+// }
 
-// Hidden properies
-const withHiddenProps = (target, prefix = '_') => {
-    return new Proxy(target, {
-        has: (obj, prop) => prop in obj && !prop.startsWith(prefix),
-        ownKeys: obj => Reflect.ownKeys(obj).filter(p => !p.startsWith(prefix)),
-        get: (obj, prop, receiver) => (prop in receiver ? obj[prop] : void 0)
-    })
-}
-
-const data = withHiddenProps({
-    name: 'Vladilen',
-    age: 25,
-    _uid: '1231231'
-})
-
-// Optimization
-const IndexedArray = new Proxy(Array, {
-    construct(target, [argArray], newTarget) {
-        const index = {}
-        argArray.forEach(item => index[item.id] = item)
-        return new Proxy(new target(...argArray), {
-            get(target, p, receiver) {
-                switch (p) {
-                    case 'push':
-                        return item => {
-                            index[item.id] = item
-                            target[p].call(target, item)
-                        }
-                    case 'findById':
-                        return id => index[id]
-                    default:
-                        return target[p]
-                }
-            }
-        })
+function* iter(n = 10) {
+    for (let i = 0; i < n; i++) {
+        yield i
     }
-})
+}
 
-const users = new IndexedArray([
-    {id: 11, name: 'Vladilen', job: 'Fullstack', age: 25},
-    {id: 22, name: 'Elena', job: 'Student', age: 22},
-    {id: 33, name: 'Victor', job: 'Backend', age: 23},
-    {id: 44, name: 'Vasilisa', job: 'Teacher', age: 24}
-])
+for (let k of iter(6)){
+    console.log(k)
+}
