426d54dd7278b3dceb9ef4f3d270585aa0659601
Sucs-God <technogen228@gmail.com>
2021-06-05 03:23:24 +0300

    [code] Lesson - 12 Array methods (forEach, map, filter, reduce, find, findIndex). Js Arrays.

--- js/index.js
+++ js/index.js
@@ -1,44 +1,78 @@
-// 11 Generators. Symbol iterator, for of
+// 12 Array methods (forEach, map, filter, reduce, find, findIndex). Js Arrays.
 
-// function* strGenerator() {
-//     yield 'H'
-//     yield 'e'
-//     yield 'l'
-//     yield 'l'
-//     yield 'o'
+const people = [
+    { name: 'Владилен', age: 25, budget: 40000},
+    { name: 'Елена', age: 17, budget: 3400},
+    { name: 'Игорь', age: 49, budget: 50000},
+    { name: 'Михаил', age: 15, budget: 1800},
+    { name: 'Василиса', age: 24, budget: 25000},
+    { name: 'Виктория', age: 38, budget: 2300}
+]
+
+// for (let i = 0; i < people.length; i++) {
+//     console.log(people[i])
 // }
-//
-// const str = strGenerator()
 
-// function* numberGen(n = 10) {
-//     for(let i = 0; i < n; i++) {
-//         yield i
+// for (let person of people) {
+//     console.log(person)
+// }
+
+// ForEach
+// people.forEach(function (person) {
+//     console.log(person)
+// })// калбек может принимать 3 аргумента
+// // 1: объект итерации
+// // 2: index
+// // 3: сам массив с которым работаем
+// people.forEach(person => console.log(person))
+
+// Map
+// const newPeople = people.map(person => person.age * 3)// калбек может принимать 3 аргумента
+// 1: объект итерации
+// 2: index
+// 3: сам массив с которым работаем
+// console.log(newPeople)
+
+// Filter
+// const adults = []
+// for (let i = 0; i < people.length; i++) {
+//     if (people[i].age >= 18) {
+//         adults.push(people[i])
 //     }
 // }
-//
-// const num = numberGen(17)
+// console.log(adults)
+// const adults = people.filter(person => person.age >= 18)//калбек может принимать 3 аргумента
+// // 1: объект итерации
+// // 2: index
+// // 3: сам массив с которым работаем
+// console.log(adults)
 
-// const iterator = {
-//     [Symbol.iterator](n = 10) {
-//         let i = 0
-//
-//         return {
-//             next() {
-//                 if (i < n) {
-//                     return {value: ++i, done: false }
-//                 }
-//                 return {value: undefined, done: true }
-//             }
-//         }
-//     }
+// Reduce
+// let amount = 0
+// for (let i = 0; i < people.length; i++) {
+//     amount += people[i].budget
 // }
+// const amount = people.reduce((total, person) => total + person.budget, 0)
+// console.log(amount)
 
-function* iter(n = 10) {
-    for (let i = 0; i < n; i++) {
-        yield i
-    }
-}
+// Find
+// const igor = people.find(person => person.name === 'Игорь')
+// console.log(igor)
 
-for (let k of iter(6)){
-    console.log(k)
-}
+// FindIndex
+// const igorIndex = people.findIndex(person => person.name === 'Игорь')
+// console.log(igorIndex)
+
+// ============
+
+const amount = people
+    .filter(person => person.budget > 3000)
+    .map(person => {
+        return {
+            info: `${person.name} (${person.age})`,
+            budget: Math.sqrt(person.budget)
+        }
+    })
+    .reduce((total, person) => total + person.budget, 0)
+
+console.log(amount)
